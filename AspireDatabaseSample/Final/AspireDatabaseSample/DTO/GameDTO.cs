ccee6cddcb9c63ccf85e5e0937e82e87b750242a
Jose María Flores Zazo <jmfloreszazo@gmail.com>
2024-06-04 15:21:48 +0200

    Add final

---
+++ AspireDatabaseSample/Final/AspireDatabaseSample/DTO/GameDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace AspireDatabaseSample.Service.DTO;
+
+public class GameDTO
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public DateTime ReleaseDate { get; set; }
+}
---
+++ AspireDatabaseSample/Final/AspireDatabaseSample/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspireDatabaseSample.Service.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Seed method call here
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<Game> Game { get; set; }
+}
